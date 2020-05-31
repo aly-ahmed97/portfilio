1517b2fc912470c88ce973001a196992eeaf0a2c
aly ahmed <65836451+aly-ahmed97@users.noreply.github.com>
2020-05-31 23:19:12 +0200

    modifications

--- js/index.js
+++ js/index.js
@@ -9,3 +9,18 @@ menuButton.addEventListener("click",function()
             nav[i].classList.toggle("mobile");
         }
     });
+
+    $(document).on("scroll", function () {
+        var pageTop = $(document).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var tags = $("article");
+        for (var i = 0; i < tags.length; i++) {
+            var tag = tags[i];
+        if ($(tag).position().top < pageBottom) { 
+            $(tag).addClass("visible")
+          }
+        else {
+            $(tag).removeClass("visible")
+          }
+        }
+    });
