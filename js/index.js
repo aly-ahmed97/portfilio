1499eaddc43ce149686abbf1f2ca169f4e6ba343
aly ahmed <65836451+aly-ahmed97@users.noreply.github.com>
2020-06-30 04:40:15 +0200

    makine a huge modification

--- js/index.js
+++ js/index.js
@@ -1,26 +1,56 @@
-let menuButton = document.getElementById("menu"),
-    nav = document.getElementsByClassName("nava"),
-    i = 0 ;
-menuButton.addEventListener("click",function()
-    {
-        
-        for(i = 0; i < 3 ; i++)
-        {
-            nav[i].classList.toggle("mobile");
-        }
-    });
+let buttons = document.getElementsByClassName("arrow"),
+    slides  = document.getElementsByClassName("slide-card"),
+    dots    = document.getElementsByClassName("black-dot"),
+    i = 0,
+    x = 0,
+    y = 0,
+    z = 0,
+    S1 = window.matchMedia("(max-width: 1199.98px)"),
+    S2 = window.matchMedia("(max-width: 575.98px)");
 
-    $(document).on("scroll", function () {
-        var pageTop = $(document).scrollTop();
-        var pageBottom = pageTop + $(window).height();
-        var tags = $("article");
-        for (var i = 0; i < tags.length; i++) {
-            var tag = tags[i];
-        if ($(tag).position().top < pageBottom) { 
-            $(tag).addClass("visible")
-          }
-        else {
-            $(tag).removeClass("visible")
-          }
+function screentest()
+{
+    if(S2.matches)
+    {
+        x = 1;
+    }
+    else if(S1.matches)
+    {
+        x = 2;
+    }
+    else
+    {
+        x = 3;
+    }
+if(y !== x)
+{
+    for(i= x ; i < slides.length ; i++)
+    {
+        slides[i].style.display = "none";
+        dots[i].classList.remove("active-dot");
+    }
+}
+}
+
+buttons[1].addEventListener("click",function()
+{
+    z++;
+    let a = 0; 
+    if ( z < (slides.length-x))
+    {
+        for(i = 0 ; i < slides.length ; i++)
+        {
+            slides[i].style.display = "none";
+            dots[i].classList.remove("active-dot");
         }
-    });
+        for(i = 0 ; i < (slides.length-x) ; i++)
+        {
+            a = i + z ;
+            console.log(a);
+            slides[a].style.display = "block";
+            dots[a].classList.add("active-dot");
+        } 
+    } 
+});
+
+screentest();
