bb71fcdf9321470ca50c6bd97e349d5b9e34aeb2
aly ahmed <65836451+aly-ahmed97@users.noreply.github.com>
2020-07-06 02:46:38 +0200

    adding ons

--- js/example1.js
+++ js/example1.js
@@ -1,3 +1,4 @@
+// google maps 
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -5,7 +6,7 @@ function initMap() {
     zoom: 17
   });
 }
-
+// text color options 
 let colors = document.getElementsByClassName("color"),
     p      = document.getElementsByTagName("p"),
     i      = 0 ,
@@ -22,12 +23,11 @@ for(i=0 ; i < colors.length ; i++)
     this.classList.remove("not-active");
     this.classList.add("active");
     let ColorText = this.classList.item(1);
-    console.log(ColorText);
-    for(y = 0 ; y < (p.length-1) ; y++)
+    for(y = 2 ; y < (p.length-1) ; y++)
       p[y].style.color = ColorText;
   }
 }
-
+//show hide menu
 let gear = document.getElementsByClassName("open-settings"),
     aside = document.getElementsByClassName("settings-section");
 
@@ -35,4 +35,31 @@ gear[0].onclick = function()
 {
   aside[0].classList.toggle("hide");
   aside[0].classList.toggle("show");
+}
+//random image
+
+let Ciamge = document.getElementById("Cimage");
+function randomImage()
+{
+let counter = Math.floor(Math.random() * 5);
+counter++;
+Ciamge.src = "./photos/pyramids"+counter+".jpg";
+}
+var intervalid = setInterval(randomImage,3000);
+
+let yes = document.getElementById("Yes"),
+    no  = document.getElementById("No");
+    
+no.onclick = function()
+{
+  if(intervalid)
+  clearInterval(intervalid);
+}
+yes.onclick = function()
+{
+    if (intervalid) {
+    clearInterval(intervalid);
+    }
+    randomImage();
+    intervalid = setInterval(randomImage,3000);
 }
